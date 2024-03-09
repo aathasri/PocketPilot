82a4d234cef7952e835f0f22cd1fde0f3b553f05
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2024-03-09 13:19:56 -0800

    node update

--- node.js
+++ node.js
@@ -2,3 +2,28 @@ const express = require("express");
 const app = express();
 const path = requires("path");
 const fs = require("fs").promises;
+
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public/icon/favicon.ico')));
+
+app.get("/", async (req, res) => {
+    try {
+        const doc = await fs.readFile(path.join(__dirname, 'app/html/main.html'), 'utf8');
+        res.send(doc);
+    } catch (error) {
+        console.error("Error reading html file:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>")
+});
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:${port}');
+});
