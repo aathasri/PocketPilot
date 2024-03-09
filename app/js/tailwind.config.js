a0837ede95f48dc4948b7b0ba453101003d8c8d2
YooranKim <yuranm@naver.com>
2024-03-09 15:22:35 -0800

    Working on Css

---
+++ app/js/tailwind.config.js
@@ -0,0 +1,19 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          lightBlue: '#DEEBFC',
+          red: '#FC6C69',
+          salmon: "#EBA8A2",
+          pink: "#FF539F",
+          blue:"#5C9AF1",
+          white: "#fff"
+        },
+        
+      },
+      fontFamily: {
+        head: ["Roboto", "sans-serif"],
+        },
+    }
+}
