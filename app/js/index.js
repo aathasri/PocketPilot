729799996a794f43887aea05bd372d4fbf3f1fee
YooranKim <yuranm@naver.com>
2024-03-09 22:38:43 -0800

    exam, assignment popup

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,7 @@
 var cBtn = document.querySelector("#courses");
+var eBtn = document.querySelector("#exam");
+var aBtn = document.querySelector("#assignment");
+
 const courseForm = () => {
     var cForm  = document.querySelector("#cForm");
     var cOpen = cForm.dataset.form;
@@ -11,5 +14,31 @@ const courseForm = () => {
         cForm.style.display = "block";
     }
 }
+const examForm = () => {
+    var eForm  = document.querySelector("#eForm");
+    var eOpen = eForm.dataset.form;
+    console.log(eOpen);
+    if(eOpen=="true"){
+        eForm.dataset.form = false;
+        eForm.style.display = "none";
+    }else{
+        eForm.dataset.form = true;
+        eForm.style.display = "block";
+    }
+}
+const assignForm = () => {
+    var aForm  = document.querySelector("#aForm");
+    var aOpen = aForm.dataset.form;
+    console.log(aOpen);
+    if(aOpen=="true"){
+        aForm.dataset.form = false;
+        aForm.style.display = "none";
+    }else{
+        aForm.dataset.form = true;
+        aForm.style.display = "block";
+    }
+}
 
 cBtn.addEventListener("click", courseForm)
+eBtn.addEventListener("click", examForm)
+aBtn.addEventListener("click", assignForm)
