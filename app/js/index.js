4c396a6c57e6cb19fe79b100c6f7a887912176ae
YooranKim <yuranm@naver.com>
2024-03-09 23:53:56 -0800

    alert and chatbot popup

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,10 @@
 var cBtn = document.querySelector("#courses");
 var eBtn = document.querySelector("#exam");
 var aBtn = document.querySelector("#assignment");
+var botBtn = document.querySelector("#botBtn");
+var botBtn02 = document.querySelector("#botBtn02");
+var closeBot = document.querySelector("#closeBot");
+var alertBtn = document.querySelector("#alertClose");
 
 const courseForm = () => {
     var cForm  = document.querySelector("#cForm");
@@ -39,6 +43,34 @@ const assignForm = () => {
     }
 }
 
+const chatbotOpen = () => {
+    var chatbot  = document.querySelector("#chatbot");
+    var botOpen = chatbot.dataset.form;
+    console.log(botOpen);
+    if(botOpen=="true"){
+        chatbot.dataset.form = false;
+        chatbot.style.display = "none";
+    }else{
+        chatbot.dataset.form = true;
+        chatbot.style.display = "block";
+    }
+}
+
+const chatbotClose = () => {
+    var chatbot  = document.querySelector("#chatbot");
+    chatbot.dataset.form = false;
+    chatbot.style.display = "none";
+}
+
+const alertClose = () => {
+    var alert  = document.querySelector("#alert");
+    alert.style.display = "none";
+}
+
 cBtn.addEventListener("click", courseForm)
 eBtn.addEventListener("click", examForm)
 aBtn.addEventListener("click", assignForm)
+botBtn.addEventListener("click", chatbotOpen)
+botBtn02.addEventListener("click", chatbotOpen)
+closeBot.addEventListener("click", chatbotClose)
+alertBtn.addEventListener("click", alertClose)
